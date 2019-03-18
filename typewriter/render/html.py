05c1cf856698549b636c04c29286bfbc4e49933d
Hamza Haiken <tenchi@team2xh.net>
2019-03-18 07:23:41 +0100

    Templatize stylesheet and simplify code

--- typewriter/render/html.py
+++ typewriter/render/html.py
@@ -2,24 +2,26 @@ import os
 import math
 import tempfile
 from yattag import Doc
+from jinja2 import Template
 
 from .furigana import html_furigana
 
 
-def resource_path(path):
-    return "file://%s/resources/%s" % (os.path.dirname(__file__), path)
+def resource_path(path, file_protocol=True):
+    protocol = "file://" if file_protocol else ""
+    return "%s%s/resources/%s" % (protocol, os.path.dirname(__file__), path)
 
 
-font_faces = """
-    @font-face {
-        font-family: "Manga Main";
-        src: url(%s);
-    }
-    @font-face {
-        font-family: "Manga Furigana";
-        src: url(%s);
-    }
-""" % (resource_path("FOT-RodinWanpakuPro-EB.otf"), resource_path("yumin.ttf"))
+resources = {
+    "font": {
+        "main": resource_path("FOT-RodinWanpakuPro-EB.otf"),
+        "furigana": resource_path("yumin.ttf")
+    },
+    "texture": resource_path("texture.png")
+}
+
+with open(resource_path("style.css.j2", file_protocol=False)) as f:
+    stylesheet_template = Template(f.read())
 
 
 class HtmlTemplate(object):
@@ -28,43 +30,47 @@ class HtmlTemplate(object):
         self.text = text
 
     def __enter__(self):
-        f = tempfile.NamedTemporaryFile(mode="w", suffix=".html", delete=False)
-        self.path = f.name
-        f.write(self.html)
-        f.close()
+        self.stylesheet = self.css()
+        self.path = self.html()
         return self.path
 
     def __exit__(self, *args):
         os.remove(self.path)
+        os.remove(self.stylesheet)
         return
 
-    @property
+    def css(self):
+        stylesheet = stylesheet_template.render(
+            image="file://" + self.template["image"],
+            **resources
+        )
+
+        f = tempfile.NamedTemporaryFile(mode="w", suffix=".css", delete=False)
+        f.write(stylesheet)
+        return f.name
+
     def html(self):
         doc, tag, txt = Doc().tagtext()
 
         with tag("html"):
-
             with tag("head"):
                 doc.stag("meta", charset="UTF-8")
-                doc.stag("link", rel="stylesheet", href=resource_path("style.css"))
-                with tag("style"):
-                    txt(font_faces)
-                    txt("body { background-image: url(%s); }" % self.template["image"])
+                doc.stag("link", rel="stylesheet", href=self.stylesheet)
 
             with tag("body"):
-                bubble = self.template["bubbles"][0]
-                style = "position: absolute; left: %d; top: %d; width: %dpx; height: %dpx;" % (
-                    bubble["x"], bubble["y"], bubble["w"], bubble["h"]
-                )
-                with tag("div", style=style, klass="bubble"):
-                    with tag("p", style="line-height: 1.5"):
-                        doc.asis(html_furigana(self.text))
+                for bubble in self.template["bubbles"]:
+                    style = "left: %d; top: %d; width: %dpx; height: %dpx;" % (
+                        bubble["x"], bubble["y"], bubble["w"], bubble["h"]
+                    )
+                    with tag("div", style=style, klass="bubble"):
+                        with tag("p"):
+                            doc.asis(html_furigana(self.text))
 
-                with tag("script", src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/textFit.js"):
+                with tag("script", src=resource_path("textFit.js")):
                     pass
                 with tag("script"):
-                    txt("""
-                        document.querySelectorAll(".bubble").forEach(textFit);
-                    """)
+                    txt("document.querySelectorAll('.bubble').forEach(textFit);")
 
-        return doc.getvalue()
+        f = tempfile.NamedTemporaryFile(mode="w", suffix=".html", delete=False)
+        f.write(doc.getvalue())
+        return f.name
