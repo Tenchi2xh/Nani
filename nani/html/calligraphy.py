9fa6bb3176492d1eb157eb95801212078ab2e80d
Hamza Haiken <tenchi@team2xh.net>
2019-04-04 22:20:48 +0200

    Add calligraphy seal template

--- nani/html/calligraphy.py
+++ nani/html/calligraphy.py
@@ -8,7 +8,8 @@ from .util import resource_path, css_position
 
 def calligraphy_template(template):
     template_type = {
-        "scroll": ScrollTemplate
+        "scroll": ScrollTemplate,
+        "seal": SealTemplate
     }
 
     return template_type.get(template["name"].split("-")[0], None)
@@ -52,3 +53,29 @@ class ScrollTemplate(HtmlTemplate):
                          klass="text fit %s" % part):
                     with tag("p"):
                         doc.asis(text)
+
+
+class SealTemplate(HtmlTemplate):
+    def __init__(self, template, text, author):
+        super().__init__(
+            template, text, author,
+            stylesheet_names=["seal.css.j2"],
+            resources={
+                "font": resource_path(template["font"]),
+                "texture": resource_path("texture.png"),
+                "bg": resource_path("templates/calligraphy/paper.png"),
+                "hue": random.randint(0, 17) * 20
+            }
+        )
+
+    def body(self, doc, tag, txt):
+        text = "".join(self.lines)
+        if len(text) == 4:
+            text = "%s%s<br>%s%s" % (text[2], text[0], text[3], text[1])
+        elif len(text) == 2:
+            text = "%s<br>%s" % (text[0], text[1])
+        else:
+            text = "?"
+        with tag("div", klass="seal"):
+            with tag("span"):
+                doc.asis(text)
